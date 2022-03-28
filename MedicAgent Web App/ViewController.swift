e6cbef489897cfc52115990a00e4ddde1e7092f8
Meena Radhakrishnan <meena.mathi@gmail.com>
2022-03-28 16:22:28 -0700

    Initial Commit

---
+++ MedicAgent Web App/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MedicAgent Web App
+//
+//  Created by Meena Radhakrishnan on 3/28/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
